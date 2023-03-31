598e464d40c74133fac5a228196a7c7a67fa0d9c
Javohir <javohir200211@gmail.com>
2023-03-31 19:57:56 +0500

    Initial commit

---
+++ 002.py
@@ -0,0 +1,5 @@
+# Challenge 002
+
+name = input("What is your firstname? ")
+surname = input("What is your surname? ")
+print(f"Hello {name} {surname}.")
---
+++ 003.py
@@ -0,0 +1,3 @@
+# Challenge 003
+
+print("What do you call a bear with no teeth?\nA gummy bear!")
