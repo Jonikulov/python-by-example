6bf27e3966a73c9d49fed918a18771d3b0a05db8
Javohir <javohir200211@gmail.com>
2023-05-01 21:32:09 +0500

    solved

--- 133.py
+++ 133.py
@@ -1,2 +1,33 @@
 # Challenge 133
 
+from tkinter import *
+
+def display_msg():
+    name = name_box.get()
+    name_box.delete(0, END)
+    msg_box['text'] = "Hello " + name
+
+root = Tk()
+root.geometry("400x350")
+root.title("Names")
+root.iconbitmap("robot.ico")
+root.config(background="black")
+
+photo = PhotoImage(file="main-image.png")
+main_image = Label(image=photo)
+main_image.place(x=20, y=10)
+
+name_label = Label(text="Enter your name:", bg='black', fg='white')
+name_label.place(x=35, y=270)
+
+name_box = Entry(width=35, justify='center')
+name_box.place(x=135, y=270)
+name_box.focus()
+
+button = Button(text="Press me", command=display_msg, bg='grey', width=12)
+button.place(x=35, y=305)
+
+msg_box = Label(width=30)
+msg_box.place(x=135, y=305)
+
+root.mainloop()
---
+++ more_tkinter.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("550x500")
+root.title("Application")
+root.wm_iconbitmap("robot.ico")
+root.config(background="light green")
+
+photo = PhotoImage(file="main-image.png")
+logoimage = Label(image=photo)
+logoimage.place(x=15, y=15)
+
+select_name = StringVar(root)
+select_name.set("Select Name")
+name_list = OptionMenu(root, select_name, "Bob", "Sue", "Tim")
+name_list.place(x=400, y=100)
+
+def clicked():
+    sel = select_name.get()
+    msg_label["text"] = "Hello " + sel
+    if sel == "Bob":
+        photo = PhotoImage(file="ai-robot.png")
+        logoimage.image = photo
+    elif sel == "Sue":
+        photo = PhotoImage(file="copilot-header.png")
+        logoimage.image = photo
+    elif sel == "Tim":
+        photo = PhotoImage(file="robot-ico.png")
+        logoimage.image = photo
+    else:
+        photo = PhotoImage(file="main-image.png")
+        logoimage.image = photo
+
+    logoimage["image"] = photo
+    logoimage.update()
+
+click_btn = Button(text="Click", command=clicked)
+click_btn.place(x=400, y=150)
+
+msg_label = Label(text="Hello")
+msg_label.place(x=400, y=200)
+
+root.mainloop()
